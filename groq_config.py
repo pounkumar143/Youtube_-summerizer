c1b6d5372dca026a811937bba21230efb63875ca
Poun kumar <143789183+pounkumar143@users.noreply.github.com>
2025-06-17 21:47:18 +0530

    groq_config.py

--- groq_config.py
+++ groq_config.py
@@ -3,6 +3,6 @@ from langchain_groq import ChatGroq
 def get_groq_llm():
     return ChatGroq(
         model="llama3-8b-8192",
-        groq_api_key ="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",
+        groq_api_key ="enter your api key",
         temperature=0.5
     )
